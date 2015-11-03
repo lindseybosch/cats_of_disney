2ceef81da0b6c46ecca7334b4a1aea2e01e37715
Lindsey Bosch <lindseybbosch@gmail.com>
2015-11-02 16:31:49 -0800

    add home page and add the ability to sign in, sign up, and sign out of account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,35 @@ def index
 
 end
 
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "You have successfully signed up!"
+      session[:user_id] = @user.id # login
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+end
 
 
+private
+
+   def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def authenticate
+    redirect_to new_session_path, alert: 'You must be logged in to continue!' if current_user.nil?
+  end
+
+  def authorize
+    @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
+  end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+root 'images#index'
 
+resources :users, only: [:index, :new, :create, :show]
 
-resources :users, only: [:index, :show]
+resource :session, only: [:new, :create, :destroy]
+
+resources :images, only: [:index]
+
+get '/login', to: 'session#new'
 end
